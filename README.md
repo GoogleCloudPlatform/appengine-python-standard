b11a260b080ec513348c650d8beebdabedc89d11
Phillip Pearson <philpearson@google.com>
2021-01-28 11:46:19 -0800

    Version 0.0.1a1
    Change-Id: I65c2c4edda98d04cfc1812e0c33db9c419d61fb1

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ runtime.
 
 In your `requirements.txt` file, add the following:
 
-`appengine-python-standard`
+`appengine-python-standard>=0.0.1a`
 
 In your app's `app.yaml`, add the following:
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="appengine-python-standard",
-    version="0.0.1",
+    version="0.0.1a1",
     author="Google LLC",
     description="Google App Engine Python 3 Standard Environment API library",
     long_description=long_description,
