faf4b3e86ea9ae3e25343ad7fa80fac87ed3a2e0
shreejad <59456116+shreejad@users.noreply.github.com>
2023-04-24 14:46:45 -0700

    Update to version 1.1.0 (#83)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="appengine-python-standard",
-    version="1.0.0",
+    version="1.1.0",
     author="Google LLC",
     description="Google App Engine services SDK for Python 3",
     long_description=long_description,
