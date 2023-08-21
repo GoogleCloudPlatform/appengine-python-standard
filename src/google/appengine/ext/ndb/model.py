546bb38707bc55f59d4e25006afe77b784791e43
peter <peter@freshplanet.com>
2023-08-21 19:59:51 -0400

    simplify + preserve existing behavior better (ie utf-8 works :P)

--- src/google/appengine/ext/ndb/model.py
+++ src/google/appengine/ext/ndb/model.py
@@ -2741,10 +2741,9 @@ class GenericProperty(Property):
         sval = modelclass._from_pb(pb)
       elif meaning != entity_pb2.Property.BYTESTRING:
         try:
-          if six.PY2:
-            sval.decode('ascii')
-          elif meaning == entity_pb2.Property.TEXT:
-            sval = sval.decode('ascii')
+          decoded = sval.decode('ascii')
+          if six.PY3 and meaning == entity_pb2.Property.TEXT:
+            sval = decoded
         except UnicodeDecodeError:
           try:
             sval = six.text_type(sval.decode('utf-8'))
