882b8fa098220636a1c259a1dfaa1e307bcfcf72
shreejad <59456116+shreejad@users.noreply.github.com>
2023-09-11 19:51:57 +0000

    Version 1.1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="appengine-python-standard",
-    version="1.1.3",
+    version="1.1.4",
     author="Google LLC",
     description="Google App Engine services SDK for Python 3",
     long_description=long_description,
