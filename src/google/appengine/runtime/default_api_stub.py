70494519e34c31841704c9db671304ee4557aef6
sharmaharisam <54685523+sharmaharisam@users.noreply.github.com>
2024-12-05 13:11:01 +0530

    Update default_api_stub.py

--- src/google/appengine/runtime/default_api_stub.py
+++ src/google/appengine/runtime/default_api_stub.py
@@ -41,7 +41,7 @@ import urllib3
 
 
 
-logging.getLogger('requests_nologs').setLevel(logging.ERROR)
+# logging.getLogger('requests_nologs').setLevel(logging.ERROR)
 
 TICKET_HEADER = 'HTTP_X_APPENGINE_API_TICKET'
 DEV_TICKET_HEADER = 'HTTP_X_APPENGINE_DEV_REQUEST_ID'
@@ -68,7 +68,34 @@ MAX_CONCURRENT_API_CALLS = 100
 
 URLLIB3_POOL_COUNT = 10
 
-URLLIB3_POOL_SIZE = 10
+POOL_SIZE_LOWER_LIMIT = 10
+POOL_SIZE_UPPER_LIMIT = 100
+POOL_SIZE_DEFAULT_VALUE = 10
+
+env_pool_size = os.environ.get('POOL_SIZE_URLLIB3')
+
+if env_pool_size is not None:
+  try:
+    URLLIB3_POOL_SIZE = int(env_pool_size)
+    logging.info(
+        f'Setting user-provided urllib3 pool size of {URLLIB3_POOL_SIZE}'
+    )
+  except ValueError:
+    logging.warning(
+        f'Invalid value for POOL_SIZE_URLLIB3: {env_pool_size}. '
+        f'Defaulting to {POOL_SIZE_DEFAULT_VALUE}.'
+    )
+    URLLIB3_POOL_SIZE = POOL_SIZE_DEFAULT_VALUE
+else:
+  URLLIB3_POOL_SIZE = POOL_SIZE_DEFAULT_VALUE
+
+if not (POOL_SIZE_LOWER_LIMIT <= URLLIB3_POOL_SIZE <= POOL_SIZE_UPPER_LIMIT):
+  logging.warning(
+      f'URLLIB3_POOL_SIZE ({URLLIB3_POOL_SIZE}) is out of range'
+      f' [{POOL_SIZE_LOWER_LIMIT}, {POOL_SIZE_UPPER_LIMIT}]. Defaulting to'
+      f' {POOL_SIZE_DEFAULT_VALUE}.'
+  )
+  URLLIB3_POOL_SIZE = POOL_SIZE_DEFAULT_VALUE
 
 
 
