2cc343333e700c820808bf0f9bec5f105341249d
sharmaharisam <54685523+sharmaharisam@users.noreply.github.com>
2024-12-06 14:06:36 +0530

    Bump version to 1.1.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="appengine-python-standard",
-    version="1.1.7",
+    version="1.1.8",
     author="Google LLC",
     description="Google App Engine services SDK for Python 3",
     long_description=long_description,
